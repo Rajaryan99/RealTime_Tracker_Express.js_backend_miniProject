1713fa6cbad8a217c557cb1747d8d5dd1ed59d0f
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-28 08:30:37 +0530

    created server using socket.io and basic setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+    res.send('hey, this is root')
+})
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+})
+
